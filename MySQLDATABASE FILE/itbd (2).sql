a9e326b5390efc6e9979fe1e8846e58fdc334016
sanjoy <sanjoykundu187@gamil.com>
2023-07-17 02:00:24 +0600

    my sql added

--- MySQLDATABASE FILE/itbd (2).sql
+++ MySQLDATABASE FILE/itbd (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 15, 2023 at 10:43 AM
+-- Generation Time: Jul 16, 2023 at 07:59 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -61,12 +61,20 @@ CREATE TABLE `courses` (
   `discount` int NOT NULL,
   `total_seats` int NOT NULL,
   `discounted_price` int NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `course_description` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `course_image` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `user_id`, `course_title`, `category_id`, `instructor_id`, `course_price`, `course_duration`, `discount`, `total_seats`, `discounted_price`, `status`, `course_description`, `course_image`, `deleted_at`, `created_at`, `updated_at`) VALUES
+(1, 5, 'Wordpress Development', 1, 1, 11000, 3, 10, 120, 9900, 'approve', 'It is one of  the freelancing course for any type of people.', '-course-wddjoxb5lhglpy8rczjv.jpg', NULL, '2023-07-16 12:40:48', '2023-07-16 13:42:39');
 
 -- --------------------------------------------------------
 
@@ -104,12 +112,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(6, '2023_06_30_095853_create_trainers_table', 3),
-(7, '2023_06_30_165700_create_success_students_table', 4),
-(10, '2023_07_02_095407_create_ourpartners_table', 5),
-(17, '2023_06_29_120036_create_courses_table', 7),
-(21, '2014_10_12_000000_create_users_table', 8),
-(22, '2023_07_02_102625_create_categories_table', 9);
+(6, '2023_06_30_095853_create_trainers_table', 2),
+(7, '2023_06_30_165700_create_success_students_table', 3),
+(10, '2023_07_02_095407_create_ourpartners_table', 4),
+(21, '2014_10_12_000000_create_users_table', 6),
+(22, '2023_07_02_102625_create_categories_table', 7),
+(23, '2023_06_29_120036_create_courses_table', 8);
 
 -- --------------------------------------------------------
 
@@ -125,13 +133,6 @@ CREATE TABLE `ourpartners` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `ourpartners`
---
-
-INSERT INTO `ourpartners` (`id`, `partner_name`, `partner_image`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Medge Patel', '-partner-ep9fekqk9cjpjmzwbe7c.png', '2023-07-03 04:01:45', '2023-07-03 04:01:45', NULL);
 
 -- --------------------------------------------------------
 
@@ -224,6 +225,13 @@ CREATE TABLE `trainers` (
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `trainers`
+--
+
+INSERT INTO `trainers` (`id`, `trainer_name`, `trainer_title`, `trainer_description`, `trainer_salary`, `trainer_facebook_link`, `trainer_linkdin_link`, `trainer_image`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'RR Raton', 'WordPress Developer', 'A highly skilled and motivated WordPress developer and trainer with 3 years of experience in designing, developing, and maintaining WordPress websites. Proficient in creating custom themes, plugins, and implementing various functionalities to enhance the user experience. Dedicated to staying up-to-date with the latest trends and technologies in the WordPress ecosystem.', 25000, 'https:://facebook1.com', 'https:://linkdin.com', 'trainer-gjnpj6l52zqsu4xy7qgm.jpg', '2023-07-16 12:31:15', '2023-07-16 12:31:15', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -252,7 +260,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `pr
 (2, 'Sanjoy Kundu', 'sanjoykundu187@gmail.com', NULL, '$2y$10$EqGodhhMHx5qEus4KHk65u5JzQYjP.TWFSlMh6DPrt9FDv4KGfj5a', NULL, 'admin', NULL, NULL, NULL),
 (3, 'Swapna Kundu', 'swapna@gmail.com', NULL, '$2y$10$6dXvqhY8SecYVhZI3pPnm.g2sdeypNyq1dXSIBbFdnUjb3V97Wy4a', NULL, 'admin', NULL, NULL, NULL),
 (4, 'Sabuj Kundu', 'sabuj@gmail.com', NULL, '$2y$10$2w1vCmnjJ8NWjrONH9GJFu18pUNFGFoGx7GtZvgGSMv71.SJ36MWS', NULL, 'vendor', NULL, NULL, NULL),
-(5, 'Ratan Bishwash', 'ratan@gmail.com', NULL, '$2y$10$hlpHrVR2.NZ4zUqhyRAelO5XR.jbyax/0Mc2BF2TIEJj1tC/.rgW2', NULL, 'vendor', NULL, NULL, NULL);
+(5, 'Ratan Bishwash', 'ratan@gmail.com', NULL, '$2y$10$hlpHrVR2.NZ4zUqhyRAelO5XR.jbyax/0Mc2BF2TIEJj1tC/.rgW2', NULL, 'vendor', NULL, NULL, NULL),
+(6, 'vendor1`', 'vendor1@gmail.com', NULL, '$2y$10$cNPyKc7xkJzlJrZvuIjLLu1jVcfOtR.958jmXOZkGigJKv7wf3VUC', NULL, 'vendor', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -344,7 +353,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -356,13 +365,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `ourpartners`
 --
 ALTER TABLE `ourpartners`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `partners`
@@ -386,13 +395,13 @@ ALTER TABLE `success_students`
 -- AUTO_INCREMENT for table `trainers`
 --
 ALTER TABLE `trainers`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
